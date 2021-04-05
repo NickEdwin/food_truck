dbcaff7e4428e3c4983e31a8be948ecf1a26c77e
Nick Edwin <nickedwin85@gmail.com>
2021-04-05 12:59:51 -0600

    Added users table and item relationship

---
+++ db/migrate/20210405184002_add_item_to_users.rb
@@ -0,0 +1,5 @@
+class AddItemToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :items, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_173813) do
+ActiveRecord::Schema.define(version: 2021_04_05_184002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,18 @@ ActiveRecord::Schema.define(version: 2021_04_05_173813) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "role", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "items", "users"
 end
